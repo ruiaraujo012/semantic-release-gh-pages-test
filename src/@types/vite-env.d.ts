99a6e8b1de55bc70e227f873e796dc45224c6bce
Rui Araújo <ruiaraujo012@gmail.com>
2023-06-25 23:26:53 +0100

    fix(types): remove some types

--- src/@types/vite-env.d.ts
+++ src/@types/vite-env.d.ts
@@ -1,13 +1,10 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-  readonly VITE_I18NEXT_DEBUG: 'true' | 'false';
-}
+// interface ImportMetaEnv {}
 
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
+// interface ImportMeta {
+// readonly env: ImportMetaEnv;
+// }
 
-declare const APP_ENVIRONMENT: string;
 declare const APP_VERSION: string;
 declare const APP_VERSION_DATE: string;
