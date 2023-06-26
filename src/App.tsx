c763ed15e68ef5f53dd11e60bbd7cc1ab99877a4
Rui Araújo <ruiaraujo012@gmail.com>
2023-06-26 23:49:55 +0100

    fix(ci): hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
 import Page1 from './Page1';
 import Page2 from './Page2';
 import Root from './Root';
 
 const App = () => {
-  const router = createBrowserRouter(
+  const router = createHashRouter(
     [
       {
         element: <Root />,
