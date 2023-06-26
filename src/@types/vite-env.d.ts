206e3bbdf1c5027ee1e84a88247450dee1252edb
Rui Araújo <ruiaraujo012@gmail.com>
2023-06-26 23:46:39 +0100

    fix(fix): fix

--- src/@types/vite-env.d.ts
+++ src/@types/vite-env.d.ts
@@ -1,10 +1,12 @@
 /// <reference types="vite/client" />
 
-// interface ImportMetaEnv {}
+interface ImportMetaEnv {
+  VITE_BASE_URL: string;
+}
 
-// interface ImportMeta {
-// readonly env: ImportMetaEnv;
-// }
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
 
 declare const APP_VERSION: string;
 declare const APP_VERSION_DATE: string;
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
-import { RouterProvider, createHashRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Page1 from './Page1';
 import Page2 from './Page2';
 import Root from './Root';
 
 const App = () => {
-  const router = createHashRouter([
+  const router = createBrowserRouter(
+    [
+      {
+        element: <Root />,
+        path: '/',
+      },
+      {
+        element: <Page1 />,
+        path: '/page1',
+      },
+      {
+        element: <Page2 />,
+        path: '/page2',
+      },
+    ],
     {
-      element: <Root />,
-      path: '/',
+      basename: import.meta.env.VITE_BASE_URL,
     },
-    {
-      element: <Page1 />,
-      path: '/page1',
-    },
-    {
-      element: <Page2 />,
-      path: '/page2',
-    },
-  ]);
+  );
 
   return <RouterProvider router={router} />;
 };
